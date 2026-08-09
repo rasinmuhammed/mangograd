78f62b168fb374031f3fd7802b1ee798a0e46caf
Muhammed Rasin <rasinbinabdulla@gmail.com>
2026-08-09 11:28:42 +0530

    Export optimizers, losses and layers from the package root

--- mangograd/__init__.py
+++ mangograd/__init__.py
@@ -1,5 +1,28 @@
-from .engine import Value
-from .tensor import Tensor
-from .nn import Linear, MLP
+"""A readable, NumPy-backed tensor autograd engine.
 
-__all__ = ['Value', 'Tensor', 'Linear', 'MLP']
+Scalar `Value` is kept for the micrograd-style walkthrough. Everything else
+is built on `Tensor`, which is vectorised and what you want in practice.
+"""
+
+from .engine import Value
+from .loss import CrossEntropyLoss, MSELoss
+from .nn import BatchNorm, Dropout, Linear, MLP, Module
+from .optim import SGD, Adam, Optimizer
+from .tensor import Tensor
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "Value",
+    "Tensor",
+    "Module",
+    "Linear",
+    "MLP",
+    "Dropout",
+    "BatchNorm",
+    "Optimizer",
+    "SGD",
+    "Adam",
+    "MSELoss",
+    "CrossEntropyLoss",
+]
